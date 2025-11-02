79359d3d12b5eeb8ac6cc386821f7c698dd1aa27
Yusuf Bakay <110858019+yb50@users.noreply.github.com>
2025-11-02 18:39:09 +0900

    feat: add keyboard & focus accessibility
    - Added keyboard navigation support: users can Tab through UI components without mouse.
    - Implemented focus management using useRef and useEffect to move focus after template changes.
    - Ensured deletion of tasks returns focus to a meaningful control (Back to Edit/Delete).
    - Checked for accessible semantics and interactive states to support screen reader users.

--- react/(2)-react-todo-app/src/components/Todo.jsx
+++ react/(2)-react-todo-app/src/components/Todo.jsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState("");
+
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
+  console.log(wasEditing);
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -27,6 +41,7 @@ function Todo(props) {
           type="text" 
           value={newName}
           onChange={handleChange}
+          ref={editFieldRef}
         />  
       </div>
 
@@ -59,7 +74,12 @@ function Todo(props) {
       </div>
 
       <div className="btn-group">
-        <button type="button" className="btn" onClick={() => setEditing(true)}>
+        <button 
+          type="button" 
+          className="btn" 
+          onClick={() => setEditing(true)}
+          ref={editButtonRef}
+        >
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
         
@@ -72,6 +92,14 @@ function Todo(props) {
       </div>
     </div>
   );
+
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    } else if (wasEditing && !isEditing) {
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
 
   return (
     <li className="todo">
